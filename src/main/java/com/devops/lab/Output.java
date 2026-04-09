4531d1a77c06710b6d62d1639dee3f7c06e39097
infinity-789 <abhay.sk002@gmail.com>
2026-04-09 10:06:15 +0530

    Create Output.java

---
+++ src/main/java/com/devops/lab/Output.java
@@ -0,0 +1,9 @@
+package com.devops.lab;
+
+public class Output {
+    
+    public static void main(String [] args) {
+        Calculator calc = new Calculator();
+        System.out.println("Sum = " + calc.add(2, 3));
+    }
+}
